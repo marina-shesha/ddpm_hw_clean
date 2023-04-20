2f8c41bcf1fc37a164cdd1427d384830fdb38ba6
marina sheshukova <sheshukovamarina06062001@gmail.com>
2023-04-20 17:15:05 +0300

    fix cuda

--- train_ddpm_cont.py
+++ train_ddpm_cont.py
@@ -2,7 +2,7 @@ from default_mnist_config import create_default_mnist_config
 from diffusion import DiffusionRunner
 
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 config = create_default_mnist_config()
 diffusion = DiffusionRunner(config)
